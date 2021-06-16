e7683204ef07bb221853b671e3effb0187fc8340
Bryson Steck <steck.bryson@gmail.com>
2021-06-15 18:51:39 -0600

    removed potion calls in seperate thread

--- src/net/brysonsteck/Resurrection/player/ResurrectPlayer.java
+++ src/net/brysonsteck/Resurrection/player/ResurrectPlayer.java
@@ -8,9 +8,6 @@ import org.bukkit.potion.PotionEffect;
 public class ResurrectPlayer {
 
     public ResurrectPlayer(Player p) {
-        for (PotionEffect effect : p.getActivePotionEffects())
-            p.removePotionEffect(effect.getType());
-        p.setGameMode(GameMode.SURVIVAL);
         Bukkit.broadcastMessage(p.getDisplayName() + " has resurrected!");
     }
 }
