dc5a958fdfb83e8cfbf057abc379a38fa8086d64
Bryson Steck <steck.bryson@gmail.com>
2021-08-11 20:50:08 -0600

    added setting parser functions

--- src/net/brysonsteck/Resurrection/ParseSettings.java
+++ src/net/brysonsteck/Resurrection/ParseSettings.java
@@ -1,5 +1,15 @@
 package net.brysonsteck.Resurrection;
 
-public class ParseSettings {
+import java.util.Hashtable;
 
+public class ParseSettings {
+    Hashtable<String, String> settings = new Hashtable<>();
+
+    public ParseSettings() {
+
+    }
+
+    public String getSetting(String setting) {
+        return settings.get(setting);
+    }
 }
